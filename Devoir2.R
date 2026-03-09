f1efe665634509332ceb0c7830f468cd039b647e
AlisonOuellet <ALOUE123@ulaval.ca>
2026-03-09 11:26:24 -0400

    b) done

--- Devoir2.R
+++ Devoir2.R
@@ -17,6 +17,7 @@ install.packages("phangorn", dependencies = TRUE)
 library(DECIPHER)
 library(ape)
 library(phangorn)
+library(Biostrings)
 
 # ----- a) Importation et alignement (DECIPHER) ------ #
 # Importation des données
@@ -35,5 +36,27 @@ aligned_no_gap <- AlignSeqs(sequences_brutes, gapOpening = 0, gapExtension = 0)
 # Visualiser l'alignement dans un browser (optionnel)
 BrowseSeqs(aligned_no_gap, highlight = 0)
 
+# Sauvegarde l'alignement final
+writeXStringSet(aligned_no_gap, "aligned_sequences.fasta")
+
 # ----- b) Traduction et cadre de lecture ------ #
 
+# Traduction avec code mitochondrial vertébré
+aligned_dna <- AlignTranslation(data,
+                                readingFrame = 1,
+                                geneticCode = getGeneticCode("2"),
+                                type = "DNAStringSet")
+
+# Pour visualiser l'alignement en AA
+aligned_aa_1 <- AlignTranslation(data,
+                                  readingFrame = 1,
+                                  geneticCode = getGeneticCode("2"),
+                                  type = "AAStringSet")
+BrowseSeqs(aligned_aa_1)
+
+# Test le cadre de lecture 2
+aligned_aa_2 <- AlignTranslation(data,
+                                  readingFrame = 2,
+                                  geneticCode = getGeneticCode("2"),
+                                  type = "AAStringSet")
+BrowseSeqs(aligned_aa_2)
