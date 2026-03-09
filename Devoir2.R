919f30f768331915ad98d7ec16c5a829f3b67a90
AlisonOuellet <ALOUE123@ulaval.ca>
2026-03-09 11:06:18 -0400

    a) done

--- Devoir2.R
+++ Devoir2.R
@@ -19,4 +19,21 @@ library(ape)
 library(phangorn)
 
 # ----- a) Importation et alignement (DECIPHER) ------ #
+# Importation des données
+data <- readDNAStringSet("Cetacea COI.fasta")
+
+# Alignement
+aligned_data <- AlignSeqs(data)
+
+# Retire les gaps de l'alignement pour retrouver les séquences brutes
+sequences_brutes <- RemoveGaps(aligned_data)
+
+# Alignement avec les pénalités à zéro
+aligned_no_gap <- AlignSeqs(sequences_brutes, gapOpening = 0, gapExtension = 0)
+
+
+# Visualiser l'alignement dans un browser (optionnel)
+BrowseSeqs(aligned_no_gap, highlight = 0)
+
+# ----- b) Traduction et cadre de lecture ------ #
 
