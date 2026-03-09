51e0f83754331ec28a4589751a223138db050e2e
AlisonOuellet <ALOUE123@ulaval.ca>
2026-03-09 10:56:19 -0400

    librarie

--- Devoir2.R
+++ Devoir2.R
@@ -3,8 +3,20 @@
 ##### BIF-4002 H26
 ############################
 
+# ------ Installation des libraries nécessaires ------ #
+
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-
 BiocManager::install("DECIPHER")
+
+install.packages('ape')
+
+install.packages("phangorn", dependencies = TRUE)
+
+
 library(DECIPHER)
+library(ape)
+library(phangorn)
+
+# ----- a) Importation et alignement (DECIPHER) ------ #
+
