85fbe32a1e6949588c72e1523e03c5be0a191c20
AlisonOuellet <ALOUE123@ulaval.ca>
2026-03-09 10:48:47 -0400

    R

---
+++ Devoir2.R
@@ -0,0 +1,10 @@
+############################
+##### PHYLOGÉNIE
+##### BIF-4002 H26
+############################
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("DECIPHER")
+library(DECIPHER)
